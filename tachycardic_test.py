94b72646682c00b00db09de5a1068e07c7bc67da
WeiHsien Lee/Willy <42806161+fffairforce@users.noreply.github.com>
2019-02-07 16:19:48 -0500

    fix syntax in testfile

--- tachycardic_test.py
+++ tachycardic_test.py
@@ -2,16 +2,16 @@ from tachycardic import is_tachycardic
 import pytest
 @pytest.mark.parametrize("a,expected",[
     ("tachycardic", True), #exact type
-    ("tacHycaRdic"), True), #acceptable typo
-    ("TachyCArdic"), True),
-    ("tachycard ic"), True), #space typo
-    ("tachyc@rd!c"), True), #punctuation typo
-    ("tach3c0rdic"), True), #numeric typo
+    ("tacHycaRdic", True), #acceptable typo
+    ("TachyCArdic", True),
+    ("tachycard ic", True), #space typo
+    ("tachyc@rd!c", True), #punctuation typo
+    ("tach3c0rdic", True), #numeric typo
     ("4526", False), #number input
     ("3.14159", False), #float number input
     ("tqcgycaedis", False), #nonsense spelling typo
     ("True", False), #bool input
-])
+    ])
 
 
 def tachycardic_test(a,expected):
