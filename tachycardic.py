c80386937e6f6c5d5ca3e8a94a4aae29d29b8e4f
WeiHsien Lee/Willy <42806161+fffairforce@users.noreply.github.com>
2019-02-06 23:41:02 -0500

    add main and user_input

--- tachycardic.py
+++ tachycardic.py
@@ -1,7 +1,18 @@
+import difflib
+def main():
+    test = is_tachycardic()
+    test_r = similarity()
+    output(test_r)
+
+def user_type():
+    in = input('Please type only string: ')
+    return in
+    
 def is_tachycardic(instring):
     print(instring)
-    import difflib
 
+def similarity(instring):
+    return difflib.SequenceMatcher(None, str(instring).lower(), "tachycardic").ratio
 
 def is_tachycardic(instring):
     r=difflib.SequenceMatcher(None, str(instring).lower(), "tachycardic").ratio()
@@ -17,4 +28,6 @@ def output(instring):
         print('exact match \ \nsimilarity:{}%'.format(round(instring,1)))
     if instring < 0.7:
         print('exact match \ \nsimilarity:{}%'.format(round(instring,1)))
-          
+
+if __name__ == "__main__":
+    main()
